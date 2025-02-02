3c0b32b3e60be870617baa7c9fb52503e0be73b0
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-02-02 18:15:24 -0500

    Add scanf example

---
+++ Chapter2/scanf_ex.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    float pi;
+
+    // read in an int value followed by a float value ("%d%g")
+    // store the int value at the memory location of x (&x)
+    // store the float value at the memory location of pi (&pi)
+    scanf("%d%g", &x, &pi);
+}
