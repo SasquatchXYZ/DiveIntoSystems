07f7740acc84c89d9659141124ecb64e8ca591b2
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-02-05 23:38:30 -0500

    Use example lib in example prog
    compiled using `gcc -o myprog myprog.c mylib.o`

---
+++ Chapter2/library/myprog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main()
+{
+    float val1, val2, ret;
+    printf("Enter two float values: ");
+    scanf("%f%f", &val1, &val2);
+    ret = bigger(val1, val2); // use a library function
+    printf("%f is the biggest\n", ret);
+
+    return 0;
+}
